3bff34ebecd215808f4fe4b0aa31360ed09f58f9
Abdelali Tigzim <atigzim@e2r13p8.1337.ma>
2025-06-28 01:29:02 +0100

    a

--- main.c
+++ main.c
@@ -7,20 +7,20 @@ void parsing(char **av, int ac, t_arg *philo)
 
 	i = 1;
 	j = 0;
-	if(ac - 1 > 5 || ac -1 < 4)
+	if (ac - 1 > 5 || ac -1 < 4)
 	{
 		write(2, "ERROR\n", 6);
 		free(philo);
 		exit(1);
 	}
-	while(av[i])
+	while (av[i])
 	{
 		j = 0;
 		while (av[i][j])
 		{
-			if(av[i][j] == '+')
+			if (av[i][j] == '+')
 				j++;
-			if(ft_isdigit(av[i][j]) == 0)
+			if (ft_isdigit(av[i][j]) == 0)
 			{
 				write(2, "ERROR\n", 6);
 				free(philo);
@@ -30,13 +30,27 @@ void parsing(char **av, int ac, t_arg *philo)
 		}
 		i++;
 	}
-	philo->philo = ft_atoi(av[1],philo);
+	philo->num_philo = ft_atoi(av[1], philo);
 	philo->t_die = ft_atoi(av[2], philo);
 	philo->t_eat = ft_atoi(av[3], philo);
 	philo->t_sleep = ft_atoi(av[4], philo);
 	if(ac - 1 ==  5)
-		philo->t_p_eat = ft_atoi(av[5], philo);
+		philo->max_meals = ft_atoi(av[5], philo);
 }
+
+
+
+// void init(t_philo *fork)
+// {
+// 	int i = 0;
+// 	while ()
+// 	{
+		
+// 	}
+	
+
+// 	fork->left_fork = 
+// }
 int main(int ac, char **av)
 {
 	t_arg *philo;
--- philo.h
+++ philo.h
@@ -5,14 +5,33 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-typedef struct s_arg
-{
-	int philo;
+typedef struct s_args {
+	int num_philo;
 	int t_die;
 	int t_eat;
 	int t_sleep;
-	int t_p_eat;
-}t_arg;
+	int max_meals; // optional
+	long start_time;
+	int someone_died;
+
+	pthread_mutex_t *forks;
+	pthread_mutex_t write_lock;
+	pthread_mutex_t dead_lock;
+	pthread_mutex_t meal_lock;
+} t_arg;
+
+typedef struct s_philo {
+	int id;
+	int meals_eaten;
+	long last_meal;
+
+	t_arg *args;
+	pthread_t thread;
+
+	pthread_mutex_t *left_fork;
+	pthread_mutex_t *right_fork;
+} t_philo;
+
 
 long	ft_atoi(const char *str, t_arg *philo);
 void parsing(char **av, int ac, t_arg *philo);
