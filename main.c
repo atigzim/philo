55b4fe940b3f76cb3fb4c78d6bdd155e67925fed
Abdelali Tigzim <atigzim@e2r11p10.1337.ma>
2025-06-30 19:35:24 +0100

    creat

--- main.c
+++ main.c
@@ -1,11 +1,30 @@
 #include "philo.h"
 
+void *my_routine()
+{ 
+	t_philo *philo;
+	
+	if (philo.)
+} 
+void creat_therad (t_philo *phelo)
+{
+	int i;
 
+	i = 0;
+	while (i < phelo->rules->nb_philo)
+	{
+		if(pthread_create(phelo->thread,NULL, my_routine,phelo) != 0)
+			perror("errore create");
+		i++;
+	}
+	
+}
 void init(t_philo *fork)
 {
 	int	i;
 	t_rules *rules;
 
+	i = 0;
 	rules = fork->rules;
 	rules->forks = malloc(sizeof(pthread_mutex_t) * rules->nb_philo);
 	if(!rules->forks)
@@ -13,11 +32,11 @@ void init(t_philo *fork)
 	while (i < rules->nb_philo)
 	{
 		if (pthread_mutex_init(&rules->forks[i], NULL) != 0)
-			return (0);
+			return;
 		i++;
 	}
 	if (pthread_mutex_init(&rules->write_lock, NULL) != 0)
-		return (0);
+		return;
 	rules->someone_died = 0;
 
 	i = 0;
@@ -25,11 +44,11 @@ void init(t_philo *fork)
 	{
 		fork->id =i ;
 		fork->meals_eaten = 0;
-		fork->last_meal = ;
+		fork->last_meal = 0;
 		fork->rules = rules;
 		fork->left_fork = &rules->forks[i];
-		fork
-		
+		fork->right_fork = &rules->forks[(i + 1) % rules->nb_philo];
+		i++;
 	}
 	
 }
@@ -41,6 +60,7 @@ int main(int ac, char **av)
 	philo = malloc(sizeof(t_philo));
 	memset(philo, 0, sizeof(t_philo));
 	parsing(av, ac, philo);
+	init(philo);
 	free(philo->rules);
 	free(philo);
 }
