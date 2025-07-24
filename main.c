1b5fa7e03ac12073d3857aff492ea1ec8dc9a1f9
Abdelali Tigzim <atigzim@c4r3p5.1337.ma>
2025-07-25 00:56:23 +0100

    routine

--- main.c
+++ main.c
@@ -6,15 +6,50 @@
 /*   By: atigzim <atigzim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 01:44:54 by atigzim           #+#    #+#             */
-/*   Updated: 2025/07/24 00:37:12 by atigzim          ###   ########.fr       */
+/*   Updated: 2025/07/25 00:51:36 by atigzim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void eating(t_rules *arg)
+{
+	t_philo *philo;
 
+	philo = arg->philo;
+	pthread_mutex_lock(&philo->right_fork);
+	pthread_mutex_lock(&philo->left_fork);
+	pthread_mutex_lock(&arg->write_lock);
+	printf("%d %s\n", philo->id, "has taken a fork");
+	printf("%d %s\n", philo->id, "eating");
+	pthread_mutex_unlock(&arg->write_lock);
+	useelp(arg->t_eat * 1000);
+	pthread_mutex_unlock(&philo->left_fork);
+	pthread_mutex_unlock(&philo->right_fork);
+}
 
+void thinking(t_rules *arg)
+{
+	t_philo *philo;
 
+	philo = arg->philo;
+	pthread_mutex_lock(&arg->write_lock);
+	printf("%d %s\n", philo->id, "thinking");
+	pthread_mutex_unlock(&arg->write_lock);
+	
+}
+void *routine()
+{
+	while (1)
+	{
+		
+	}
+	
+}
+void	create_thread()
+{
+	
+}
 
 int	main(int ac, char **av)
 {
@@ -24,6 +59,6 @@ int	main(int ac, char **av)
 	memset(arg, 0, sizeof(t_rules));
 	parsing(av, ac, arg);
 	init_all(arg);
-	printf("%ld\n", get_time_ms());
+	// printf("%ld\n", get_time_ms());
 	free(arg);
 }
