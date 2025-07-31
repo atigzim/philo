776ab94ee90652c7187fac6b19851caa8a660ea9
Abdelali Tigzim <atigzim@c4r3p5.1337.ma>
2025-07-31 21:48:40 +0100

    creat

--- philo/main.c
+++ philo/main.c
@@ -1,4 +1,35 @@
 #include "philo.h"
+void *routine(void *arg)
+{
+	t_philo *philo;
+	int		i;
+
+	philo = (t_philo *)arg;
+	i = 0;
+	while (1)
+	{
+		
+	}
+	
+}
+void create_thread(t_rules *arg)
+{
+	int i;
+	t_philo *philo;
+
+	i = 0;
+	philo = arg->philo;
+	while (i < arg->nb_philo)
+	{
+		philo[i].start_time = get_time_ms();
+		if ((pthread_create(&philo[i].thread, NULL, routine, &philo[i])) != 0)
+		{
+			printf("Failed to create thread for philo %d\n", i + 1);
+			return ;
+		}
+	}
+	
+}
 
 int main(int ac , char **av)
 {
@@ -7,4 +38,5 @@ int main(int ac , char **av)
 	arg = malloc(sizeof(t_rules));
 	memset(arg, 0, sizeof(t_rules));
 	parsing(av, ac, arg);
+	init_all(arg);
 }
